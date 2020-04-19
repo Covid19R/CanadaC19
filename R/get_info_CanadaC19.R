ab0ada096b553301718c3d8b5dabf8a0f440c0a2
Morgan DeBusk-Lane <mldebusklane@gmail.com>
2020-04-19 09:51:10 -0400

    Clean

--- R/get_info_CanadaC19.R
+++ R/get_info_CanadaC19.R
@@ -13,29 +13,27 @@
 #' }
 #'
 get_info_CanadaC19 <- function() {
-  latest_data <- 
+  latest_data <-
     refresh_CanadaC19_cases(verbose = FALSE)
 
-  dplyr::tribble(
+  dplyr::tibble(
     data_set_name = "CanadaC19_cases",
     package_name = "CanadaC19",
     function_to_get_data = "refresh_CanadaC19_cases",
     data_details = "Open Source data from multiple public reporting data throughout Canada. For more, see https://github.com/ishaberry/Covid19Canada.",
     data_url = "https://raw.githubusercontent.com/ishaberry/Covid19Canada/master/cases.csv",
     license_url = "https://github.com/debusklaneml/CanadaC19/blob/master/LICENSE",
-    data_types =
+    data_type =
       latest_data %>%
-      tidyr::drop_na(data_type) %>%
-      dplyr::pull(data_type) %>%
-      unique() %>%
-      stringr::str_c(collapse = ", "),
-    location_types =
+        tidyr::drop_na(data_type) %>%
+        dplyr::pull(data_type) %>%
+        unique(),
+    location_type =
       latest_data %>%
-      tidyr::drop_na(location_type) %>%
-      dplyr::pull(location_type) %>%
-      unique() %>%
-      stringr::str_c(collapse = ", "),
-    spatial_extent = "country",
+        tidyr::drop_na(location_type) %>%
+        dplyr::pull(location_type) %>%
+        unique(),
+    spatial_extent = "state",
     has_geospatial_info = FALSE
   )
 }
